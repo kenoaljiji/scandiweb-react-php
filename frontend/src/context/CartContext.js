14aa1c95f0ca87ee5ca15156a967a76b952bc45c
kenoaljiji <keno.a89@gmail.com>
2024-08-23 12:26:28 +0200

    delete json file and add CartContext component

---
+++ frontend/src/context/CartContext.js
@@ -0,0 +1,181 @@
+import React, { createContext, Component } from 'react';
+import client from '../services/apollo-client';
+import { PLACE_ORDER_MUTATION } from './queries';
+
+export const CartContext = createContext();
+
+export class CartProvider extends Component {
+  state = {
+    cartItems: [],
+    totalItems: 0,
+    total: 0,
+    isCartOpen: false,
+    loading: false,
+  };
+
+  componentDidMount() {
+    const storedCartItems = localStorage.getItem('cartItems');
+    if (storedCartItems) {
+      const cartItems = JSON.parse(storedCartItems);
+      this.setState({ cartItems }, () => {
+        this.calculateTotal();
+        this.calculateTotalItems();
+      });
+    }
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    if (prevState.cartItems !== this.state.cartItems) {
+      localStorage.setItem('cartItems', JSON.stringify(this.state.cartItems));
+    }
+  }
+
+  // Add to cart or increase quantity
+  addToCart = (item) => {
+    const existingItemIndex = this.state.cartItems.findIndex(
+      (cartItem) =>
+        cartItem.id === item.id &&
+        JSON.stringify(cartItem.selectedAttributes) ===
+          JSON.stringify(item.selectedAttributes)
+    );
+
+    if (existingItemIndex !== -1) {
+      // Item exists, increase quantity
+      const newCartItems = [...this.state.cartItems];
+      newCartItems[existingItemIndex].quantity += 1;
+      this.setState({ cartItems: newCartItems }, () => {
+        this.calculateTotal();
+        this.calculateTotalItems();
+      });
+    } else {
+      // New item, set initial quantity and add to cart
+      item.quantity = 1;
+      this.setState({ cartItems: [...this.state.cartItems, item] }, () => {
+        this.calculateTotal();
+        this.calculateTotalItems();
+      });
+    }
+  };
+
+  // Remove from cart or decrease quantity
+  removeFromCart = (id, selectedAttributes) => {
+    const existingItemIndex = this.state.cartItems.findIndex(
+      (item) =>
+        item.id === id &&
+        JSON.stringify(item.selectedAttributes) ===
+          JSON.stringify(selectedAttributes)
+    );
+
+    if (existingItemIndex !== -1) {
+      const newCartItems = [...this.state.cartItems];
+      const item = newCartItems[existingItemIndex];
+
+      if (item.quantity > 1) {
+        // Decrease quantity
+        item.quantity -= 1;
+        this.setState({ cartItems: newCartItems }, () => {
+          this.calculateTotal();
+          this.calculateTotalItems();
+        });
+      } else {
+        // Remove item entirely
+        newCartItems.splice(existingItemIndex, 1);
+        this.setState({ cartItems: newCartItems }, () => {
+          this.calculateTotal();
+          this.calculateTotalItems();
+        });
+      }
+    }
+  };
+
+  calculateTotal = () => {
+    const total = this.state.cartItems.reduce(
+      (acc, item) => acc + item.quantity * item.prices[0].amount,
+      0
+    );
+    this.setState({ total });
+  };
+
+  calculateTotalItems = () => {
+    const totalItems = this.state.cartItems.reduce(
+      (acc, item) => acc + item.quantity,
+      0
+    );
+    this.setState({ totalItems });
+  };
+
+  //Throlle a click
+
+  throttle = (func, limit) => {
+    let inThrottle;
+    return (...args) => {
+      if (!inThrottle) {
+        func.apply(this, args);
+        inThrottle = true;
+        setTimeout(() => (inThrottle = false), limit);
+      }
+    };
+  };
+
+  toggleCart = this.throttle(() => {
+    this.setState((prevState) => ({
+      isCartOpen: !prevState.isCartOpen,
+    }));
+  }, 100); // Adjust the delay as needed
+
+  // Show the cart overlay
+  showCart = () => {
+    this.setState({ isCartOpen: true });
+  };
+
+  // Function to place an order
+  placeOrder = async () => {
+    const orderItems = this.state.cartItems.map((item) => ({
+      productId: item.id,
+      productName: item.name,
+      quantity: item.quantity,
+      price: item.prices[0].amount,
+      selectedAttributes: JSON.stringify(item.selectedAttributes),
+    }));
+
+    try {
+      this.setState({ loading: true });
+
+      const { data } = await client.mutate({
+        mutation: PLACE_ORDER_MUTATION,
+        variables: { items: orderItems },
+      });
+
+      if (data.insertOrder.success) {
+        this.setState({ cartItems: [], total: 0, totalItems: 0 }); // Clear the cart
+        console.log(
+          'Order placed successfully with ID:',
+          data.insertOrder.orderId
+        );
+      } else {
+        console.error('Order placement failed:', data.insertOrder.message);
+      }
+    } catch (error) {
+      console.error('Error placing order:', error);
+    }
+    this.setState({ loading: false });
+  };
+
+  render() {
+    return (
+      <CartContext.Provider
+        value={{
+          ...this.state,
+          addToCart: this.addToCart,
+          removeFromCart: this.removeFromCart,
+          toggleCart: this.toggleCart,
+          placeOrder: this.placeOrder,
+          showCart: this.showCart,
+          hideCart: this.hideCart,
+        }}
+      >
+        {this.props.children}
+      </CartContext.Provider>
+    );
+  }
+}
